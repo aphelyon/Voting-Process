621823563a48b05c58a1f08762c63213cd43b5df
arun8r <arun8r@gmail.com>
2018-03-02 15:43:54 -0500

    travis and tests

--- vote/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-./vote/vote_1register/web/manage.py test ./vote/vote_1register/web/tests
-./vote/vote_1register/proc/manage.py test ./vote/vote_1register/proc/tests
---
+++ vote/vote_1register/proc/tests.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./manage.py test ./vote/vote_1register/web/tests
