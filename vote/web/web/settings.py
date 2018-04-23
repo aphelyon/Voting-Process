59ddc2282aa5ddeb24fafdd2e3ce286a6b2d660e
sed5qg <sed5qg@virginia.edu>
2018-04-23 18:03:36 -0400

    finished nav panel

--- vote/web/web/settings.py
+++ vote/web/web/settings.py
@@ -71,10 +71,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-             'libraries':{
-                'increment': 'web.templatetags.increment',
-
-            }
         },
     },
 ]
